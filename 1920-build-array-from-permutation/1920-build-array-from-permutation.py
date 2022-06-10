f7219bddb0ed850e31f477b7f9609a589f9ecc29
Nicholas Yoo <sodlalwl13@gmail.com>
2022-06-11 00:02:40 +0900

    Time: 192 ms (31.53%), Space: 14.2 MB (6.62%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.py
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.py
@@ -3,7 +3,8 @@ class Solution:
         n = len(nums)
         for i in range(n):
             nums[i] = nums[i] + (n *(nums[nums[i]]%n))
+            # 현재 위치에 현재 위치값 + 전체 길이 * 현재 위치값이 있는 위치값을 전체 길이로 나눈 값
         for i in range(n):
             nums[i] = int(nums[i]/n)
-        
+            # 위에서 전체 길이로 곱하였기때문에 다시 전체 길이로 나누어준다.
         return nums
