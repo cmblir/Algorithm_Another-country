5d84dc51a28f623949d61254238c37e63b4e7c3a
Nicholas Yoo <sodlalwl13@gmail.com>
2022-06-29 13:12:29 +0900

    Time: 59 ms (24.37%), Space: 13.9 MB (17.68%) - LeetHub

--- 1805-number-of-different-integers-in-a-string/1805-number-of-different-integers-in-a-string.py
+++ 1805-number-of-different-integers-in-a-string/1805-number-of-different-integers-in-a-string.py
@@ -1,9 +1,5 @@
+import re
 class Solution:
     def numDifferentIntegers(self, word: str) -> int:
-        alpha = [i for i in "abcdefghijklnmopqrstuvwxyz"]
-        for i in word:
-            if i in alpha:
-                word = word.replace(i, " ")
-        tmp = word.split()
-        tmp = [int(i) for i in tmp]
-        return len(list(set(tmp))) # 중복을 제거한 리스트의 길이
+        word = re.sub('[a-zA-Z]',' ', word)
+        return len(list(set(int(i) for i in word.split())))
