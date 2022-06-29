b515c0ac77b3bdcdccee791ffb221417d8def07d
Nicholas Yoo <sodlalwl13@gmail.com>
2022-06-29 13:14:11 +0900

    Time: 53 ms (39.08%), Space: 13.9 MB (17.68%) - LeetHub

--- 1805-number-of-different-integers-in-a-string/1805-number-of-different-integers-in-a-string.py
+++ 1805-number-of-different-integers-in-a-string/1805-number-of-different-integers-in-a-string.py
@@ -2,4 +2,4 @@ import re
 class Solution:
     def numDifferentIntegers(self, word: str) -> int:
         word = re.sub('[a-zA-Z]',' ', word)
-        return len(list(set(int(i) for i in word.split())))
+        return len(set(int(i) for i in word.split()))
